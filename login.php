a6ba91b0a61d67d43bd724abb7a7ed572ca5bab0
Haroone <88834685+Haroone@users.noreply.github.com>
2022-01-29 21:57:06 +0100

    Update login.php

--- login.php
+++ login.php
@@ -12,7 +12,7 @@
         <div class="row">
             <div class="col-md-4 offset-md-4">
                 <div class="login-form bg-light mt-4 p-4">
-                    <form action="" method="" class="row g-3">
+                    <form action="" method="post" class="row g-3">
                         <h4>Welcome Back</h4>
                         <div class="col-12">
                             <label>Username</label>
@@ -29,7 +29,7 @@
                             </div>
                         </div>
                         <div class="col-12">
-                            <button type="submit" class="btn btn-dark float-end">Login</button>
+                            <button type="submit" name="L" class="btn btn-dark float-end">Login</button>
                         </div>
                     </form>
                     <hr class="mt-4">
